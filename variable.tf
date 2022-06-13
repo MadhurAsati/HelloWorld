b76f1bc83fb6da7d858c0374fd144fce060ad6ae
MadhurAsati <madhur9827@gmail.com>
2022-06-13 10:14:49 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,25 @@
 
+variable "resource_group_name" {
+  type = string
+  default = "testrg1"
+}
+variable "location" {
+  type = string
+  default = "west europe"
+}
+variable "subnet_name" {
+  type = string
+  default = ""
+}
+variable "nic_name" {
+  type = string
+  default = ""
+}
+variable "vm_name" {
+  type = string
+  default = ""
+}
+variable "count_vm" {
+  type = number 
+  default = 4
+}
